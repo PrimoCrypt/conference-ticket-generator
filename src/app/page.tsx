615302c6a94a632eb023282154533c1e03d8f85a
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2025-02-13 19:31:15 +0100

    fix: inital intial od the form not displayed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AttendeeFormHeader from "./_components/AttendeeFormHeader";
 import AttendeeForm from "./_components/AttendeeForm";
 
 const Page = () => {
-	const [formProgress, setFormProgress] = useState<number>(100);
+	const [formProgress, setFormProgress] = useState<number>(33);
 	return (
 		<div className=" w-full flex flex-col items-center"><div className={`border border-solid rounded-[3rem] border-primary bg-[#041E23]  ${formProgress===33 ? "py-6": "py-8"} ${formProgress===100 && "mt-10"} p-6  lg:p-12 flex flex-col gap-8   w-full max-w-[700px] `}>
 		<AttendeeFormHeader formProgress={formProgress} />
